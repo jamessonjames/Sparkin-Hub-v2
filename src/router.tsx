e9e463f39d2a528cb106b07b66858c90cfb4e9f0
James <james@users.noreply.github.com>
2026-07-28 18:02:07 -0300

    perf: reduce global staleTime to 5min

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,7 @@ export const getRouter = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 30 * 60 * 1000,
+        staleTime: 5 * 60 * 1000,
         gcTime: 30 * 60 * 1000,
       },
     },
